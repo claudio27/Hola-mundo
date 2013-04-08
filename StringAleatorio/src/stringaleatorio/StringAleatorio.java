33724d0f9a1f5557cb14c63e573dd8b4341ba658
17705280 <17705280@VK1-LAB119.allnet.ucentral.cl>
2013-04-08 20:08:50 -0300

    Manejo de cadenas de texto aleatorias

---
+++ StringAleatorio/src/stringaleatorio/StringAleatorio.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringaleatorio;
+
+/**
+ *
+ * @author 17705280
+ */
+public class StringAleatorio {
+
+  
+       
+      
+public String cadenaAleatoria(int tam){
+
+     String cadena = "ABCDEFGHIJKLMUNOPQRSTUVXYZ123456789";
+       String resultado= "";
+
+for(int i = 0; i < tam; i++){
+
+ resultado += cadena.charAt((int)(Math.random()*33));
+
+}
+
+return resultado;
+
+
+}
+  
+    public static void main(String[] args) {
+        
+       StringAleatorio st = new StringAleatorio();
+       
+       String cad = st.cadenaAleatoria(20);
+       
+       System.out.println(cad);
+}
+}
